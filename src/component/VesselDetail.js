b103d9e34ed00682e94ad0e4b969f4549793b17f
marugy <gyulife7301@naver.com>
2022-11-16 17:33:54 +0900

    feat: 선박에 있는 블럭 조회 filter

--- src/component/VesselDetail.js
+++ src/component/VesselDetail.js
@@ -8,8 +8,32 @@ const VesselDetail = () => {
   const [blockList, setBlockList] = useState({});
   const [enrollModalOpen, setEnrollModalOpen] = useState(false);
 
-  useEffect(() => {
-    fetch(`http://34.64.185.37:8080/v2/block/list?imo=${state.imo}`, {
+  const blockAdd = () => {
+    setEnrollModalOpen(true);
+  };
+
+  const [searchInfo, setsearchInfo] = useState({
+    blockName: "",
+    workingStep: "",
+  });
+
+  const selectList = [
+    "",
+    "PieceAssembly",
+    "SubBlockAssembly",
+    "BlockAssembly",
+    "INSTALL",
+  ];
+
+  const handleChangeSearch = (e) => {
+    setsearchInfo({
+      ...searchInfo,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = () => {
+    fetch(`http://34.64.185.37:8080/v2/block/list?imo=${state.imo}&block_name=${searchInfo.blockName}&working_step=${searchInfo.workingStep}`, {
       method: "GET",
       headers: {
         Authorization: localStorage.getItem("access_token"),
@@ -19,10 +43,6 @@ const VesselDetail = () => {
       .then((result) => {
         setBlockList(result);
       });
-  }, []);
-
-  const blockAdd = () => {
-    setEnrollModalOpen(true);
   };
 
   return (
@@ -38,6 +58,36 @@ const VesselDetail = () => {
         <br/>착공일 : {state.startDate}
         <br/>준공일(준공예정일) : {state.endDate}
       </div>
+
+
+
+
+      <div className="Lookup">
+        <h2>선박에 등록되어 있는 블럭 조회</h2>
+        검색하실 블럭 명을 입력하세요 :{" "}
+        <input
+          type={"text"}
+          name="blockName"
+          value={searchInfo.blockName}
+          onChange={handleChangeSearch}
+          placeholder={"XX블럭"}
+        />
+        <br />
+        검색하실 작업단계를 입력하세요 :{" "}
+        <select
+          onChange={handleChangeSearch}
+          value={searchInfo.workingStep}
+          name="workingStep"
+        >
+          {selectList.map((item) => (
+            <option value={item} key={item}>
+              {item}
+            </option>
+          ))}
+        </select>
+        <button onClick={handleSubmit}>검색</button>
+      </div>
+
       <BlockList
         blockList={blockList.blockInfoList}
         btnType="detail"
