a21fd92de178e82ff71fd1502a4daf73e67abab4
marugy <gyulife7301@naver.com>
2022-11-21 12:49:39 +0900

    style: UserInfo 직업 한글 맵핑

--- src/component/UserInfo.js
+++ src/component/UserInfo.js
@@ -1,9 +1,12 @@
 const UserInfo = ({ userdata }) => {
+  const clientTypeList = {
+    INSPECTOR: "검사관",
+    MANUFURER: "제조업자",
+  };
+
   return (
     <div className="UserInfo">
-      <div className="sys_name">
-        V-Check
-      </div>
+      <div className="sys_name">V-Check</div>
       <div className="user_img">
         <img
           src={`${process.env.PUBLIC_URL}/assets/person.png`}
@@ -15,7 +18,7 @@ const UserInfo = ({ userdata }) => {
         <div>소속: {userdata.belongs}</div>
         <div>직책: {userdata.duty}</div>
         <div>이메일: {userdata.email}</div>
-        <div>직업: {userdata.clientType}</div>
+        <div>직업: {clientTypeList[userdata.clientType]}</div>
       </div>
     </div>
   );
